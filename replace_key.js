baf348ec00af6448e9d1abbcd45a734d09f4479a
Toruz3 <129600097+Toruz3@users.noreply.github.com>
2026-03-31 17:23:36 +0200

    reset

--- replace_key.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const file = 'src/server/gemini.ts';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/process\.env\.API_KEY \|\| process\.env\.GEMINI_API_KEY/g, '"AIzaSyCjHU1JzTNVDPGXsZkJy4TZKbf6oGrOPMQ"');
-fs.writeFileSync(file, content);
